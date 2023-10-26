1f9dce9e4bdfd06c615182c90864725da239c095
danielbejaranov <debejara@espol.edu.ec>
2023-10-26 14:48:58 -0500

    Other classes created with getters, setter and constructor.

---
+++ src/Class.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class Class {
+    private int id;
+    private String name;
+    private String classroom;
+    private ArrayList<Student> students;
+    private Teacher teacher;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Class(int id, String name, String classroom, Teacher teacher) {
+        this.id = id;
+        this.name = name;
+        this.classroom = classroom;
+        this.teacher = teacher;
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void addGroupOfStudents(ArrayList<Student> groupOfStudents) {
+        students.addAll(groupOfStudents);
+    }
+
+}
